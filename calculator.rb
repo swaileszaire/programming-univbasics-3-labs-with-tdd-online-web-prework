4219aefee2becb953e9679cd29eb8fc0eaa175e7
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-10 04:14:29 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number = get_variable_from_file('./calculator.rb', "first_number")
+expect(first_number).to 
+expect(second_number).not_to equal (0)
+be_an(Integer).or
+be_a(Float)
+end
